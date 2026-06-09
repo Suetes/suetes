05422bcfcad915ac5512460a325281174b9b7157
Alex Bihlo <abihlo@mun.ca>
2026-06-09 15:05:49 -0230

    Update GMRES tolerances and update plots

--- suetes/regional3d/steppers.py
+++ suetes/regional3d/steppers.py
@@ -336,7 +336,7 @@ class SemiImplicitSolver3D:
                 'eta_dot': L_out['eta_dot'] 
             }
 
-        x_sol_scaled, info = gmres(A_fn, rhs_scaled, x0=rhs_scaled, tol=1e-6, maxiter=20, restart=10, M=M_fn)
+        x_sol_scaled, info = gmres(A_fn, rhs_scaled, x0=rhs_scaled, tol=1e-4, maxiter=20, restart=20, M=M_fn)
         
         return {
             'u': x_sol_scaled['u'], 
